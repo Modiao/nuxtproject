3b46a4f540d6519c451cfa7d0f32834000cf7f86
Modou Diao <modou.diao@wizall.com>
2021-03-18 16:51:05 +0000

    first push for nuxt

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+<main>
+  <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (lien interne vers une page appartenant à notre application Nuxt)
+    </NuxtLink>
+</main>
+</template>
